43964f3e827beeb48ec71912cb420643e003587e
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-04-10 17:08:05 +0300

    All information is shown on the screen. There is a problem with loading images.

--- MakeYourMargarita/Services/NetworkManager.swift
+++ MakeYourMargarita/Services/NetworkManager.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UIKit
+
+class NetworkManager {
+    
+    let shared = NetworkManager()
+    
+    static func fetchData(_ completion: @escaping (Drink) -> ()) {
+        
+        guard let url = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "Something went wrong")
+                return
+            }
+            do {
+                let drink = try JSONDecoder().decode(Drink.self, from: data)
+                completion(drink)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+        
+    }
+    
+    static func fetchImage(_ margaritasImageURL: String?, completion: @escaping (UIImage) -> ()) {
+        guard let url = URL(string: margaritasImageURL ?? "") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response else {
+                print(error?.localizedDescription ?? "Something went wrong")
+                return
+            }
+            print(response)
+            guard let image = UIImage(data: data) else { return }
+            completion(image)
+        }.resume()
+    }
+    
+    private init() {}
+    
+}
+
+
+
+
+
--- MakeYourMargarita/ViewControllers/MargaritasTypesTableViewController.swift
+++ MakeYourMargarita/ViewControllers/MargaritasTypesTableViewController.swift
@@ -8,82 +8,59 @@
 import UIKit
 
 class MargaritasTypesTableViewController: UITableViewController {
+    
+    // MARK: - Public Properties
+    var margaritas: [Margarita] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    var margaritasImages: [UIImage?] = []
 
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        NetworkManager.fetchData() { drink in
+            self.margaritas += drink.drinks
+            self.updateImages()
+        }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    // MARK: - Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return margaritas.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MargaritasDescriptionCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let margarita = margaritas[indexPath.row]
+        content.text = margarita.strDrink ?? ""
+        content.secondaryText = margarita.composition
+        content.image = UIImage(named: "BaseImage2")
+        content.imageProperties.cornerRadius = 30
+        cell.contentConfiguration = content
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 180
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    private func updateImages() {
+        margaritas.forEach { margarita in
+            NetworkManager.fetchImage(margarita.strDrinkThumb) { image in
+                self.margaritasImages.append(image)
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+ 
 }
