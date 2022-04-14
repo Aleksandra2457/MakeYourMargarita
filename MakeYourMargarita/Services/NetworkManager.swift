8552c3ad2e6f79799dfb76abbe11791d24dd0a21
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-04-14 03:34:30 +0300

    The latest version of the project.

--- MakeYourMargarita/Services/NetworkManager.swift
+++ MakeYourMargarita/Services/NetworkManager.swift
@@ -20,30 +20,6 @@ class NetworkManager {
     
     private init() {}
     
-    func fetchData(_ completion: @escaping (Drink) -> ()) {
-        
-        guard let url = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita") else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "Something went wrong")
-                return
-            }
-            
-            do {
-                let drink = try JSONDecoder().decode(Drink.self, from: data)
-                print(drink)
-                DispatchQueue.main.async {
-                    completion(drink)
-                }
-            } catch let error {
-                print(error.localizedDescription)
-            }
-            
-        }.resume()
-        
-    }
-    
     func fetchImage(from url: String?, completion: @escaping (Data) -> Void)  {
         guard let stringURL = url else { return }
         guard let imageURL = URL(string: stringURL) else { return }
